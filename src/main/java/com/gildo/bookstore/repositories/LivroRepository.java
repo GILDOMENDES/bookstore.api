feb49db2514df6fa01258b04af9e3ce485c5baec
GILDOMENDES <odlig29@gmail.com>
2021-01-25 13:34:34 -0300

    repositoreis implementados

---
+++ src/main/java/com/gildo/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.gildo.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gildo.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
